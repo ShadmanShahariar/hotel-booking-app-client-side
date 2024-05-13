4df1f1aa2491988150821c894de9b83a99cae8d1
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2024-05-13 21:40:08 +0600

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/home/Home';
+import { List } from './components/list/List';
+import { Hotel } from './components/hotel/Hotel';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/hotels",
+    element: <List/>,
+  },
+  {
+    path: "/hotels/:id",
+    element: <Hotel/>,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
